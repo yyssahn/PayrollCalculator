3d69bdf79b5c2c5feefb3bfc99728386b5832cff
Yoonsung Ahn <yyssahn@hotmail.com>
2017-01-01 09:16:39 -0800

    dd

---
+++ src/controller/EIStateListener.java
@@ -0,0 +1,22 @@
+package controller;
+
+import gui.EiCppPanel;
+
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+
+public class EIStateListener implements ItemListener {
+	
+	EiCppPanel mPanel;
+	
+	public EIStateListener(EiCppPanel panel){
+		 mPanel = panel;		
+	}
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == )
+	}
+
+}
--- src/gui/EiCppPanel.java
+++ src/gui/EiCppPanel.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Panel;
 
+import javax.swing.BorderFactory;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 
@@ -26,10 +27,22 @@ public class EiCppPanel extends Panel{
 	private void setUI(){
 		eiExemptLabel = new JLabel("EI EXEMPT?");
 		eiExemptLabel.setPreferredSize(new Dimension(120,20));
+		eiExemptLabel.setBorder(BorderFactory.createEmptyBorder(0,0,0,10));
+		eiExemptLabel.setHorizontalAlignment(JLabel.LEFT);
 		this.add(eiExemptLabel);
+		eiExemptCheckBox = new JCheckBox();
+
+		
+		
+		this.add(eiExemptCheckBox);
+		
+		
 		
 		cppExemptLabel = new JLabel("CPP EXEMPT?");
 		cppExemptLabel.setPreferredSize(new Dimension(120,20));
+		cppExemptLabel.setBorder(BorderFactory.createEmptyBorder(0,0,0,10));
+		cppExemptLabel.setHorizontalAlignment(JLabel.LEFT);
+		
 		this.add(cppExemptLabel);
 	}
 	private void setListener(){
