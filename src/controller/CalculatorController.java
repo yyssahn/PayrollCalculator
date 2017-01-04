cdafc0828baab3c0f465338d4d00540f86eac3fe
Yoonsung Ahn <yyssahn@hotmail.com>
2017-01-04 08:23:32 -0800

    d

--- src/controller/CalculatorController.java
+++ src/controller/CalculatorController.java
@@ -9,20 +9,28 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JOptionPane;
 
+import constants.BCClaimCodes;
+import constants.FederalClaimCodes;
+
 public class CalculatorController implements ActionListener{
 	MainPanel mainPanel;
 	EiCppPanel eiCppPanel;
 	ClaimPanel claimPanel;
-	
+	FederalClaimCodes federalCode;
+	BCClaimCodes provincialCode;
 	public CalculatorController(MainPanel main, EiCppPanel eicpp, ClaimPanel claim){
 		mainPanel = main;
 		eiCppPanel = eicpp;
 		claimPanel = claim;
+		federalCode = FederalClaimCodes.ZERO;
+		provincialCode = BCClaimCodes.ZERO;
 	}
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		// TODO Auto-generated method stub
-		
+		if (event.getSource() == mainPanel.getCalculateButton()){
+			System.out.printLn()
+		}
 	}
 
 }
